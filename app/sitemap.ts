9e366b2409fd48b908023cc5d88138f7910952ae
Balemdag0 <burakalemdag@icloud.com>
2026-02-21 22:10:35 +0300

    Update domain name to ozlemotocam.com in sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://ozlemotocamtrabzon.com',
+            url: 'https://ozlemotocam.com',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
